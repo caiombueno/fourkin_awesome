a037f8980a4c366bdd5f26b0facd0a921d4a3952
Caio Bueno <caiomeirabueno@hotmail.com>
2024-09-06 09:56:35 -0300

    remove API key

--- app.config.js
+++ app.config.js
@@ -5,6 +5,13 @@ export default {
         slug: "fourkin-awesome",
         extra: {
             yelpApiToken: process.env.YELP_API_TOKEN,
+            firebaseApiKey: process.env.FIREBASE_API_KEY,
+            firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
+            firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
+            firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+            firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+            firebaseAppId: process.env.FIREBASE_APP_ID,
+            firebaseMeasurementId: process.env.FIREBASE_MEASUREMENT_ID
         },
         "ios": {
             "bundleIdentifier": "com.yourcompany.yourapp"  // Add this line
--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,19 +3,22 @@ import { initializeApp } from "firebase/app";
 import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import Constants from 'expo-constants';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const env = Constants.expoConfig?.extra;
+
 const firebaseConfig = {
-    apiKey: "AIzaSyC_tbrO8CqHq1nwhwAZ4er_VS8LKF44LRQ",
-    authDomain: "fourkinawesome.firebaseapp.com",
-    projectId: "fourkinawesome",
-    storageBucket: "fourkinawesome.appspot.com",
-    messagingSenderId: "158340021056",
-    appId: "1:158340021056:web:bdfc7da67d971021a898a5",
-    measurementId: "G-80LV1GMKF3"
+    apiKey: env.firebaseApiKey,
+    authDomain: env.firebaseAuthDomain,
+    projectId: env.firebaseProjectId,
+    storageBucket: env.firebaseStorageBucket,
+    messagingSenderId: env.firebaseMessagingSenderId,
+    appId: env.firebaseAppId,
+    measurementId: env.firebaseMeasurementId
 };
 
 // Initialize Firebase
